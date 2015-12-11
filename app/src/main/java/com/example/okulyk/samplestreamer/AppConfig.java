361b70ea36d1f8d82c0a8e06249b02004830607c
OKulyk <OKulyk@luxoft.com>
2015-12-11 13:40:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/okulyk/samplestreamer/AppConfig.java
@@ -0,0 +1,7 @@
+package com.example.okulyk.samplestreamer;
+
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://54.84.241.223:1935/live/myStream";
+    public static final String PUBLISHER_USERNAME = "d";
+    public static final String PUBLISHER_PASSWORD = "1";
+}
